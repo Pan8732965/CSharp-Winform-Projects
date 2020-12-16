029513db1db42e6fb310d7bfb96c8359e0e9f5e2
Jay Pan <smartjay1206@gmail.com>
2020-12-16 22:42:40 +0800

    enter

---
+++ Calculator_gui_project/new_calculator_gui_project/new_calculator_gui_project/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿
+namespace new_calculator_gui_project
+{
+    partial class Calculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculator));
+            this.showing_numbers = new System.Windows.Forms.TextBox();
+            this.zero_button = new System.Windows.Forms.Button();
+            this.one_button = new System.Windows.Forms.Button();
+            this.two_button = new System.Windows.Forms.Button();
+            this.three_button = new System.Windows.Forms.Button();
+            this.dot_button = new System.Windows.Forms.Button();
+            this.minus_button = new System.Windows.Forms.Button();
+            this.equal_button = new System.Windows.Forms.Button();
+            this.plus_button = new System.Windows.Forms.Button();
+            this.four_button = new System.Windows.Forms.Button();
+            this.seven_button = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.five_button = new System.Windows.Forms.Button();
+            this.eight_button = new System.Windows.Forms.Button();
+            this.six_button = new System.Windows.Forms.Button();
+            this.nine_button = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.divide_button = new System.Windows.Forms.Button();
+            this.multipy_button = new System.Windows.Forms.Button();
+            this.remainder_button = new System.Windows.Forms.Button();
+            this.clear_all_button = new System.Windows.Forms.Button();
+            this.clear_front_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // showing_numbers
+            // 
+            this.showing_numbers.Location = new System.Drawing.Point(51, 25);
+            this.showing_numbers.Multiline = true;
+            this.showing_numbers.Name = "showing_numbers";
+            this.showing_numbers.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.showing_numbers.Size = new System.Drawing.Size(589, 62);
+            this.showing_numbers.TabIndex = 0;
+            // 
+            // zero_button
+            // 
+            this.zero_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.zero_button.Location = new System.Drawing.Point(51, 478);
+            this.zero_button.Name = "zero_button";
+            this.zero_button.Size = new System.Drawing.Size(213, 73);
+            this.zero_button.TabIndex = 1;
+            this.zero_button.Text = "0";
+            this.zero_button.UseVisualStyleBackColor = true;
+            // 
+            // one_button
+            // 
+            this.one_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.one_button.Location = new System.Drawing.Point(51, 389);
+            this.one_button.Name = "one_button";
+            this.one_button.Size = new System.Drawing.Size(94, 83);
+            this.one_button.TabIndex = 19;
+            this.one_button.Text = "1";
+            this.one_button.UseVisualStyleBackColor = true;
+            // 
+            // two_button
+            // 
+            this.two_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.two_button.Location = new System.Drawing.Point(164, 389);
+            this.two_button.Name = "two_button";
+            this.two_button.Size = new System.Drawing.Size(100, 83);
+            this.two_button.TabIndex = 20;
+            this.two_button.Text = "2";
+            this.two_button.UseVisualStyleBackColor = true;
+            // 
+            // three_button
+            // 
+            this.three_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.three_button.Location = new System.Drawing.Point(281, 389);
+            this.three_button.Name = "three_button";
+            this.three_button.Size = new System.Drawing.Size(90, 83);
+            this.three_button.TabIndex = 23;
+            this.three_button.Text = "3";
+            this.three_button.UseVisualStyleBackColor = true;
+            // 
+            // dot_button
+            // 
+            this.dot_button.BackColor = System.Drawing.Color.DarkOrchid;
+            this.dot_button.Font = new System.Drawing.Font("思源黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dot_button.ForeColor = System.Drawing.Color.Transparent;
+            this.dot_button.Location = new System.Drawing.Point(281, 478);
+            this.dot_button.Name = "dot_button";
+            this.dot_button.Size = new System.Drawing.Size(90, 73);
+            this.dot_button.TabIndex = 24;
+            this.dot_button.Text = ".";
+            this.dot_button.UseVisualStyleBackColor = false;
+            // 
+            // minus_button
+            // 
+            this.minus_button.Font = new System.Drawing.Font("思源黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.minus_button.Location = new System.Drawing.Point(391, 478);
+            this.minus_button.Name = "minus_button";
+            this.minus_button.Size = new System.Drawing.Size(103, 73);
+            this.minus_button.TabIndex = 25;
+            this.minus_button.Text = "-";
+            this.minus_button.UseVisualStyleBackColor = true;
+            // 
+            // equal_button
+            // 
+            this.equal_button.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.equal_button.Font = new System.Drawing.Font("思源黑體 Heavy", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.equal_button.Location = new System.Drawing.Point(520, 389);
+            this.equal_button.Name = "equal_button";
+            this.equal_button.Size = new System.Drawing.Size(120, 162);
+            this.equal_button.TabIndex = 26;
+            this.equal_button.Text = "=";
+            this.equal_button.UseVisualStyleBackColor = false;
+            // 
+            // plus_button
+            // 
+            this.plus_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.plus_button.Location = new System.Drawing.Point(391, 389);
+            this.plus_button.Name = "plus_button";
+            this.plus_button.Size = new System.Drawing.Size(103, 83);
+            this.plus_button.TabIndex = 27;
+            this.plus_button.Text = "+";
+            this.plus_button.UseVisualStyleBackColor = true;
+            // 
+            // four_button
+            // 
+            this.four_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.four_button.Location = new System.Drawing.Point(51, 289);
+            this.four_button.Name = "four_button";
+            this.four_button.Size = new System.Drawing.Size(94, 83);
+            this.four_button.TabIndex = 28;
+            this.four_button.Text = "4";
+            this.four_button.UseVisualStyleBackColor = true;
+            // 
+            // seven_button
+            // 
+            this.seven_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.seven_button.Location = new System.Drawing.Point(51, 195);
+            this.seven_button.Name = "seven_button";
+            this.seven_button.Size = new System.Drawing.Size(94, 83);
+            this.seven_button.TabIndex = 29;
+            this.seven_button.Text = "7";
+            this.seven_button.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button9.Location = new System.Drawing.Point(51, 110);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(213, 69);
+            this.button9.TabIndex = 30;
+            this.button9.Text = "Log";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // five_button
+            // 
+            this.five_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.five_button.Location = new System.Drawing.Point(164, 289);
+            this.five_button.Name = "five_button";
+            this.five_button.Size = new System.Drawing.Size(100, 83);
+            this.five_button.TabIndex = 31;
+            this.five_button.Text = "5";
+            this.five_button.UseVisualStyleBackColor = true;
+            // 
+            // eight_button
+            // 
+            this.eight_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.eight_button.Location = new System.Drawing.Point(164, 195);
+            this.eight_button.Name = "eight_button";
+            this.eight_button.Size = new System.Drawing.Size(100, 83);
+            this.eight_button.TabIndex = 32;
+            this.eight_button.Text = "8";
+            this.eight_button.UseVisualStyleBackColor = true;
+            // 
+            // six_button
+            // 
+            this.six_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.six_button.Location = new System.Drawing.Point(281, 289);
+            this.six_button.Name = "six_button";
+            this.six_button.Size = new System.Drawing.Size(90, 83);
+            this.six_button.TabIndex = 33;
+            this.six_button.Text = "6";
+            this.six_button.UseVisualStyleBackColor = true;
+            // 
+            // nine_button
+            // 
+            this.nine_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nine_button.Location = new System.Drawing.Point(281, 195);
+            this.nine_button.Name = "nine_button";
+            this.nine_button.Size = new System.Drawing.Size(90, 83);
+            this.nine_button.TabIndex = 34;
+            this.nine_button.Text = "9";
+            this.nine_button.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button14.Location = new System.Drawing.Point(281, 110);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(90, 69);
+            this.button14.TabIndex = 35;
+            this.button14.Text = "√";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button15.Location = new System.Drawing.Point(391, 110);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(103, 69);
+            this.button15.TabIndex = 36;
+            this.button15.Text = "x²";
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // divide_button
+            // 
+            this.divide_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.divide_button.Location = new System.Drawing.Point(391, 195);
+            this.divide_button.Name = "divide_button";
+            this.divide_button.Size = new System.Drawing.Size(103, 83);
+            this.divide_button.TabIndex = 37;
+            this.divide_button.Text = "÷";
+            this.divide_button.UseVisualStyleBackColor = true;
+            // 
+            // multipy_button
+            // 
+            this.multipy_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.multipy_button.Location = new System.Drawing.Point(391, 289);
+            this.multipy_button.Name = "multipy_button";
+            this.multipy_button.Size = new System.Drawing.Size(103, 83);
+            this.multipy_button.TabIndex = 38;
+            this.multipy_button.Text = "×";
+            this.multipy_button.UseVisualStyleBackColor = true;
+            // 
+            // remainder_button
+            // 
+            this.remainder_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.remainder_button.Location = new System.Drawing.Point(520, 289);
+            this.remainder_button.Name = "remainder_button";
+            this.remainder_button.Size = new System.Drawing.Size(120, 83);
+            this.remainder_button.TabIndex = 39;
+            this.remainder_button.Text = "%";
+            this.remainder_button.UseVisualStyleBackColor = true;
+            // 
+            // clear_all_button
+            // 
+            this.clear_all_button.BackColor = System.Drawing.Color.Red;
+            this.clear_all_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.clear_all_button.ForeColor = System.Drawing.Color.Transparent;
+            this.clear_all_button.Location = new System.Drawing.Point(520, 195);
+            this.clear_all_button.Name = "clear_all_button";
+            this.clear_all_button.Size = new System.Drawing.Size(120, 83);
+            this.clear_all_button.TabIndex = 40;
+            this.clear_all_button.Text = "C";
+            this.clear_all_button.UseVisualStyleBackColor = false;
+            // 
+            // clear_front_button
+            // 
+            this.clear_front_button.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.clear_front_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.clear_front_button.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.clear_front_button.Location = new System.Drawing.Point(520, 110);
+            this.clear_front_button.Name = "clear_front_button";
+            this.clear_front_button.Size = new System.Drawing.Size(120, 69);
+            this.clear_front_button.TabIndex = 41;
+            this.clear_front_button.Text = "CE";
+            this.clear_front_button.UseVisualStyleBackColor = false;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(678, 575);
+            this.Controls.Add(this.clear_front_button);
+            this.Controls.Add(this.clear_all_button);
+            this.Controls.Add(this.remainder_button);
+            this.Controls.Add(this.multipy_button);
+            this.Controls.Add(this.divide_button);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.nine_button);
+            this.Controls.Add(this.six_button);
+            this.Controls.Add(this.eight_button);
+            this.Controls.Add(this.five_button);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.seven_button);
+            this.Controls.Add(this.four_button);
+            this.Controls.Add(this.plus_button);
+            this.Controls.Add(this.equal_button);
+            this.Controls.Add(this.minus_button);
+            this.Controls.Add(this.dot_button);
+            this.Controls.Add(this.three_button);
+            this.Controls.Add(this.two_button);
+            this.Controls.Add(this.one_button);
+            this.Controls.Add(this.zero_button);
+            this.Controls.Add(this.showing_numbers);
+            this.Name = "Calculator";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox showing_numbers;
+        private System.Windows.Forms.Button zero_button;
+        private System.Windows.Forms.Button one_button;
+        private System.Windows.Forms.Button two_button;
+        private System.Windows.Forms.Button three_button;
+        private System.Windows.Forms.Button dot_button;
+        private System.Windows.Forms.Button minus_button;
+        private System.Windows.Forms.Button equal_button;
+        private System.Windows.Forms.Button plus_button;
+        private System.Windows.Forms.Button four_button;
+        private System.Windows.Forms.Button seven_button;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button five_button;
+        private System.Windows.Forms.Button eight_button;
+        private System.Windows.Forms.Button six_button;
+        private System.Windows.Forms.Button nine_button;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button divide_button;
+        private System.Windows.Forms.Button multipy_button;
+        private System.Windows.Forms.Button remainder_button;
+        private System.Windows.Forms.Button clear_all_button;
+        private System.Windows.Forms.Button clear_front_button;
+    }
+}
+

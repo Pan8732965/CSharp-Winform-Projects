97f2c7fd50749416b55c2bfde3998b9b5ea8f16f
Jay Pan <smartjay1206@gmail.com>
2020-12-19 15:50:28 +0800

    enter

--- Calculator_gui_project/new_calculator_gui_project/new_calculator_gui_project/Form1.Designer.cs
+++ Calculator_gui_project/new_calculator_gui_project/new_calculator_gui_project/Form1.Designer.cs
@@ -60,7 +60,7 @@ namespace new_calculator_gui_project
             // result
             // 
             this.result.Font = new System.Drawing.Font("Microsoft JhengHei UI", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.result.Location = new System.Drawing.Point(51, 25);
+            this.result.Location = new System.Drawing.Point(49, 107);
             this.result.Multiline = true;
             this.result.Name = "result";
             this.result.RightToLeft = System.Windows.Forms.RightToLeft.No;
@@ -73,120 +73,130 @@ namespace new_calculator_gui_project
             // zero_button
             // 
             this.zero_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.zero_button.Location = new System.Drawing.Point(51, 478);
+            this.zero_button.Location = new System.Drawing.Point(49, 560);
             this.zero_button.Name = "zero_button";
             this.zero_button.Size = new System.Drawing.Size(213, 73);
             this.zero_button.TabIndex = 1;
             this.zero_button.Text = "0";
             this.zero_button.UseVisualStyleBackColor = true;
             this.zero_button.Click += new System.EventHandler(this.one_button_Click);
+            this.zero_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // one_button
             // 
             this.one_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.one_button.Location = new System.Drawing.Point(51, 389);
+            this.one_button.Location = new System.Drawing.Point(49, 471);
             this.one_button.Name = "one_button";
             this.one_button.Size = new System.Drawing.Size(94, 83);
             this.one_button.TabIndex = 19;
             this.one_button.Text = "1";
             this.one_button.UseVisualStyleBackColor = true;
             this.one_button.Click += new System.EventHandler(this.one_button_Click);
+            this.one_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // two_button
             // 
             this.two_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.two_button.Location = new System.Drawing.Point(164, 389);
+            this.two_button.Location = new System.Drawing.Point(162, 471);
             this.two_button.Name = "two_button";
             this.two_button.Size = new System.Drawing.Size(100, 83);
             this.two_button.TabIndex = 20;
             this.two_button.Text = "2";
             this.two_button.UseVisualStyleBackColor = true;
             this.two_button.Click += new System.EventHandler(this.one_button_Click);
+            this.two_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // three_button
             // 
             this.three_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.three_button.Location = new System.Drawing.Point(281, 389);
+            this.three_button.Location = new System.Drawing.Point(279, 471);
             this.three_button.Name = "three_button";
             this.three_button.Size = new System.Drawing.Size(90, 83);
             this.three_button.TabIndex = 23;
             this.three_button.Text = "3";
             this.three_button.UseVisualStyleBackColor = true;
             this.three_button.Click += new System.EventHandler(this.one_button_Click);
+            this.three_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // dot_button
             // 
             this.dot_button.BackColor = System.Drawing.Color.DarkOrchid;
             this.dot_button.Font = new System.Drawing.Font("思源黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.dot_button.ForeColor = System.Drawing.Color.Transparent;
-            this.dot_button.Location = new System.Drawing.Point(281, 478);
+            this.dot_button.Location = new System.Drawing.Point(279, 560);
             this.dot_button.Name = "dot_button";
             this.dot_button.Size = new System.Drawing.Size(90, 73);
             this.dot_button.TabIndex = 24;
             this.dot_button.Text = ".";
             this.dot_button.UseVisualStyleBackColor = false;
             this.dot_button.Click += new System.EventHandler(this.one_button_Click);
+            this.dot_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // minus_button
             // 
             this.minus_button.Font = new System.Drawing.Font("思源黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.minus_button.Location = new System.Drawing.Point(391, 478);
+            this.minus_button.Location = new System.Drawing.Point(389, 560);
             this.minus_button.Name = "minus_button";
             this.minus_button.Size = new System.Drawing.Size(103, 73);
             this.minus_button.TabIndex = 25;
             this.minus_button.Text = "-";
             this.minus_button.UseVisualStyleBackColor = true;
             this.minus_button.Click += new System.EventHandler(this.operation_click);
+            this.minus_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // equal_button
             // 
             this.equal_button.BackColor = System.Drawing.Color.MediumTurquoise;
             this.equal_button.Font = new System.Drawing.Font("思源黑體 Heavy", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.equal_button.Location = new System.Drawing.Point(520, 389);
+            this.equal_button.Location = new System.Drawing.Point(518, 471);
             this.equal_button.Name = "equal_button";
             this.equal_button.Size = new System.Drawing.Size(120, 162);
             this.equal_button.TabIndex = 26;
             this.equal_button.Text = "=";
             this.equal_button.UseVisualStyleBackColor = false;
             this.equal_button.Click += new System.EventHandler(this.equal_button_Click);
+            this.equal_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // plus_button
             // 
             this.plus_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.plus_button.Location = new System.Drawing.Point(391, 389);
+            this.plus_button.Location = new System.Drawing.Point(389, 471);
             this.plus_button.Name = "plus_button";
             this.plus_button.Size = new System.Drawing.Size(103, 83);
             this.plus_button.TabIndex = 27;
             this.plus_button.Text = "+";
             this.plus_button.UseVisualStyleBackColor = true;
             this.plus_button.Click += new System.EventHandler(this.operation_click);
+            this.plus_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // four_button
             // 
             this.four_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.four_button.Location = new System.Drawing.Point(51, 289);
+            this.four_button.Location = new System.Drawing.Point(49, 371);
             this.four_button.Name = "four_button";
             this.four_button.Size = new System.Drawing.Size(94, 83);
             this.four_button.TabIndex = 28;
             this.four_button.Text = "4";
             this.four_button.UseVisualStyleBackColor = true;
             this.four_button.Click += new System.EventHandler(this.one_button_Click);
+            this.four_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // seven_button
             // 
             this.seven_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.seven_button.Location = new System.Drawing.Point(51, 195);
+            this.seven_button.Location = new System.Drawing.Point(49, 277);
             this.seven_button.Name = "seven_button";
             this.seven_button.Size = new System.Drawing.Size(94, 83);
             this.seven_button.TabIndex = 29;
             this.seven_button.Text = "7";
             this.seven_button.UseVisualStyleBackColor = true;
             this.seven_button.Click += new System.EventHandler(this.one_button_Click);
+            this.seven_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // button9
             // 
             this.button9.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button9.Location = new System.Drawing.Point(51, 110);
+            this.button9.Location = new System.Drawing.Point(49, 192);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(213, 69);
             this.button9.TabIndex = 30;
@@ -196,51 +206,55 @@ namespace new_calculator_gui_project
             // five_button
             // 
             this.five_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.five_button.Location = new System.Drawing.Point(164, 289);
+            this.five_button.Location = new System.Drawing.Point(162, 371);
             this.five_button.Name = "five_button";
             this.five_button.Size = new System.Drawing.Size(100, 83);
             this.five_button.TabIndex = 31;
             this.five_button.Text = "5";
             this.five_button.UseVisualStyleBackColor = true;
             this.five_button.Click += new System.EventHandler(this.one_button_Click);
+            this.five_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // eight_button
             // 
             this.eight_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.eight_button.Location = new System.Drawing.Point(164, 195);
+            this.eight_button.Location = new System.Drawing.Point(162, 277);
             this.eight_button.Name = "eight_button";
             this.eight_button.Size = new System.Drawing.Size(100, 83);
             this.eight_button.TabIndex = 32;
             this.eight_button.Text = "8";
             this.eight_button.UseVisualStyleBackColor = true;
             this.eight_button.Click += new System.EventHandler(this.one_button_Click);
+            this.eight_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // six_button
             // 
             this.six_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.six_button.Location = new System.Drawing.Point(281, 289);
+            this.six_button.Location = new System.Drawing.Point(279, 371);
             this.six_button.Name = "six_button";
             this.six_button.Size = new System.Drawing.Size(90, 83);
             this.six_button.TabIndex = 33;
             this.six_button.Text = "6";
             this.six_button.UseVisualStyleBackColor = true;
             this.six_button.Click += new System.EventHandler(this.one_button_Click);
+            this.six_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // nine_button
             // 
             this.nine_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.nine_button.Location = new System.Drawing.Point(281, 195);
+            this.nine_button.Location = new System.Drawing.Point(279, 277);
             this.nine_button.Name = "nine_button";
             this.nine_button.Size = new System.Drawing.Size(90, 83);
             this.nine_button.TabIndex = 34;
             this.nine_button.Text = "9";
             this.nine_button.UseVisualStyleBackColor = true;
             this.nine_button.Click += new System.EventHandler(this.one_button_Click);
+            this.nine_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // button14
             // 
             this.button14.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button14.Location = new System.Drawing.Point(281, 110);
+            this.button14.Location = new System.Drawing.Point(279, 192);
             this.button14.Name = "button14";
             this.button14.Size = new System.Drawing.Size(90, 69);
             this.button14.TabIndex = 35;
@@ -250,7 +264,7 @@ namespace new_calculator_gui_project
             // button15
             // 
             this.button15.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button15.Location = new System.Drawing.Point(391, 110);
+            this.button15.Location = new System.Drawing.Point(389, 192);
             this.button15.Name = "button15";
             this.button15.Size = new System.Drawing.Size(103, 69);
             this.button15.TabIndex = 36;
@@ -261,29 +275,31 @@ namespace new_calculator_gui_project
             // divide_button
             // 
             this.divide_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.divide_button.Location = new System.Drawing.Point(391, 195);
+            this.divide_button.Location = new System.Drawing.Point(389, 277);
             this.divide_button.Name = "divide_button";
             this.divide_button.Size = new System.Drawing.Size(103, 83);
             this.divide_button.TabIndex = 37;
             this.divide_button.Text = "÷";
             this.divide_button.UseVisualStyleBackColor = true;
             this.divide_button.Click += new System.EventHandler(this.operation_click);
+            this.divide_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // multipy_button
             // 
             this.multipy_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.multipy_button.Location = new System.Drawing.Point(391, 289);
+            this.multipy_button.Location = new System.Drawing.Point(389, 371);
             this.multipy_button.Name = "multipy_button";
             this.multipy_button.Size = new System.Drawing.Size(103, 83);
             this.multipy_button.TabIndex = 38;
             this.multipy_button.Text = "×";
             this.multipy_button.UseVisualStyleBackColor = true;
             this.multipy_button.Click += new System.EventHandler(this.operation_click);
+            this.multipy_button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keyPressed);
             // 
             // remainder_button
             // 
             this.remainder_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.remainder_button.Location = new System.Drawing.Point(520, 289);
+            this.remainder_button.Location = new System.Drawing.Point(518, 371);
             this.remainder_button.Name = "remainder_button";
             this.remainder_button.Size = new System.Drawing.Size(120, 83);
             this.remainder_button.TabIndex = 39;
@@ -296,7 +312,7 @@ namespace new_calculator_gui_project
             this.clear_all_button.BackColor = System.Drawing.Color.Red;
             this.clear_all_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.clear_all_button.ForeColor = System.Drawing.Color.Transparent;
-            this.clear_all_button.Location = new System.Drawing.Point(520, 195);
+            this.clear_all_button.Location = new System.Drawing.Point(518, 277);
             this.clear_all_button.Name = "clear_all_button";
             this.clear_all_button.Size = new System.Drawing.Size(120, 83);
             this.clear_all_button.TabIndex = 40;
@@ -309,7 +325,7 @@ namespace new_calculator_gui_project
             this.clear_front_button.BackColor = System.Drawing.Color.MediumTurquoise;
             this.clear_front_button.Font = new System.Drawing.Font("思源黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.clear_front_button.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.clear_front_button.Location = new System.Drawing.Point(520, 110);
+            this.clear_front_button.Location = new System.Drawing.Point(518, 192);
             this.clear_front_button.Name = "clear_front_button";
             this.clear_front_button.Size = new System.Drawing.Size(120, 69);
             this.clear_front_button.TabIndex = 41;
@@ -332,7 +348,7 @@ namespace new_calculator_gui_project
             this.showing_quation.AutoSize = true;
             this.showing_quation.BackColor = System.Drawing.Color.White;
             this.showing_quation.Font = new System.Drawing.Font("思源黑體 Light", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
-            this.showing_quation.Location = new System.Drawing.Point(51, 25);
+            this.showing_quation.Location = new System.Drawing.Point(49, 107);
             this.showing_quation.Name = "showing_quation";
             this.showing_quation.Size = new System.Drawing.Size(0, 29);
             this.showing_quation.TabIndex = 43;
@@ -343,7 +359,7 @@ namespace new_calculator_gui_project
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(678, 575);
+            this.ClientSize = new System.Drawing.Size(678, 649);
             this.Controls.Add(this.showing_quation);
             this.Controls.Add(this.show_equation);
             this.Controls.Add(this.clear_front_button);
@@ -369,9 +385,10 @@ namespace new_calculator_gui_project
             this.Controls.Add(this.one_button);
             this.Controls.Add(this.zero_button);
             this.Controls.Add(this.result);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Calculator";
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.Text = "Form1";
+            this.Text = "Calculator++";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Calculator_gui_project/new_calculator_gui_project/new_calculator_gui_project/Form1.cs
+++ Calculator_gui_project/new_calculator_gui_project/new_calculator_gui_project/Form1.cs
@@ -20,6 +20,8 @@ namespace new_calculator_gui_project
         //problem:1.when entered the equal for the first turn,the showing number can still input the number
         //當按完等號，顯示的數字仍然可以繼續輸入數字
         //problem:2.the functions of sqrt and log can't use
+        //problem:3.when from entered the number to pressed the equal button,and pressed the operator it went wrong on the textbox.
+        //problem:4.and a lot that idk how to describe...
         public Calculator()
         {
             InitializeComponent();
@@ -33,27 +35,42 @@ namespace new_calculator_gui_project
             }
             operation_clicked = false;
             Button b = (Button)sender;  // create new object to get numbers
+            if (b.Text == ".")
+            {
+                if(!result.Text.Contains("."))
+                    result.Text = result.Text + b.Text; //print the result
+            }else
             result.Text = result.Text + b.Text;
         }
 
-        private void clear_all_button_Click(object sender, EventArgs e)
+        private void clear_all_button_Click(object sender, EventArgs e)//ce button
         {
             result.Clear();
             value = 0;
             result.Text = "0";
+            showing_quation.Text = " ";
         }
 
-        private void operation_click(object sender, EventArgs e)
+        private void operation_click(object sender, EventArgs e) //operation button
         {
             Button b = (Button)sender;
-            operation = b.Text;
-            value =double.Parse(result.Text);
-            operation_clicked = true;
-            showing_quation.Text = value + "" + operation;
-
+            if (value!=0)
+            {
+                equal_button.PerformClick();
+                operation_clicked = true;
+                operation = b.Text;
+                show_equation.Text = value + " " + operation;
+            }
+            else
+            {
+                operation = b.Text;
+                value =double.Parse(result.Text);
+                operation_clicked = true;
+                showing_quation.Text = value + "" + operation;
+            }
         }
 
-        private void equal_button_Click(object sender, EventArgs e)
+        private void equal_button_Click(object sender, EventArgs e) //equal button
         {
             showing_quation.Text = "";
             
@@ -74,17 +91,21 @@ namespace new_calculator_gui_project
                 case "%":
                     result.Text = (value % double.Parse(result.Text)).ToString();
                     break;
-
+                
                 default:
                     break;
             }// finsihed the switch
+            //value = Int32.Parse(result.Text);
+            //operation = "";
             operation_clicked = false;
+
 
         }
 
-        private void clear_front_button_Click(object sender, EventArgs e)
+        private void clear_front_button_Click(object sender, EventArgs e) //c button
         {
             result.Text = "0";
+            showing_quation.Text = " ";
         }
 
         private void result_TextChanged(object sender, EventArgs e)
@@ -92,7 +113,7 @@ namespace new_calculator_gui_project
 
         }
 
-        private void funtion_clicked(object sender, EventArgs e)
+        private void funtion_clicked(object sender, EventArgs e) //log、sqrt、x^2 button
         {
             operation_clicked = false;
             Button b = (Button)sender;
@@ -113,7 +134,61 @@ namespace new_calculator_gui_project
                     break;
             }
             
+        }
 
+        private void keyPressed(object sender, KeyPressEventArgs e)  //use keypad to enter
+        {
+            switch (e.KeyChar.ToString())
+            {
+                case "1":
+                    one_button.PerformClick();
+                    break;
+                case "2":
+                    two_button.PerformClick();
+                    break;
+                case "3":
+                    three_button.PerformClick();
+                    break;
+                case "4":
+                    four_button.PerformClick();
+                    break;
+                case "5":
+                    five_button.PerformClick();
+                    break;
+                case "6":
+                    six_button.PerformClick();
+                    break;
+                case "7":
+                    seven_button.PerformClick();
+                    break;
+                case "8":
+                    eight_button.PerformClick();
+                    break;
+                case "9":
+                    nine_button.PerformClick();
+                    break;
+                case "0":
+                    zero_button.PerformClick();
+                    break;
+                case "+":
+                    plus_button.PerformClick();
+                    break;
+                case "-":
+                    minus_button.PerformClick();
+                    break;
+                case "*":
+                    multipy_button.PerformClick();
+                    break;
+                case "/":
+                    divide_button.PerformClick();
+                    break;
+                case "=":
+                    equal_button.PerformClick();
+                    showing_quation.Text = "";
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
